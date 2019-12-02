f49a63608a7ff94d056fb09a31fddab45216b67f
Seth Corker <seth.corker@gmail.com>
2019-12-02 16:36:59 +0000

    run through tut

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { motion, useCycle } from "framer-motion";
 import Workspace from "@darth-knoppix/react-phone-show";
+import { motion, useCycle } from "framer-motion";
+
 import sereja from "./images/sereja-ris-zGyG2OyLu4k-unsplash.jpg";
 import husen from "./images/husen-siraaj-zyJkh98ySE0-unsplash.jpg";
 import houston from "./images/houston-ray-bkZlpn5iMEM-unsplash.jpg";
@@ -11,26 +12,14 @@ import Card from "./Card";
 import Header from "./Header";
 
 function App() {
-  const [animate, onToggleFocus] = useCycle(
+  const [animate, toggleFocus] = useCycle(
     {
-      container: {
-        height: "25rem",
-        top: "0rem",
-        overflowX: "auto"
-      },
-      title: {
-        opacity: 1
-      }
+      container: { height: "25rem", top: "0rem", overflowX: "auto" },
+      title: { opacity: 1 }
     },
     {
-      container: {
-        height: "100%",
-        top: "-4.35rem",
-        overflowX: "hidden"
-      },
-      title: {
-        opacity: 0
-      }
+      container: { height: "100%", top: "-4.4rem", overflowX: "hidden" },
+      title: { opacity: 0 }
     }
   );
 
@@ -39,31 +28,23 @@ function App() {
       <motion.h1
         className="title"
         animate={animate.title}
-        transition={{ ease: "easeIn" }}
+        transition={{ ease: "easeInOut", duration: 0.2 }}
       >
         Showcase
       </motion.h1>
       <motion.div
         className="card-container"
         animate={animate.container}
-        transition={{ ease: "easeOut" }}
-        onToggleFocus={onToggleFocus}
+        initial={{ top: "0rem" }}
+        transition={{ ease: "easeOut", delay: 0.1 }}
       >
-        <Card title="Sereja Ris" image={sereja} onToggleFocus={onToggleFocus} />
-        <Card
-          title="Husen Siraaj"
-          image={husen}
-          onToggleFocus={onToggleFocus}
-        />
-        <Card
-          title="Houston Ray"
-          image={houston}
-          onToggleFocus={onToggleFocus}
-        />
+        <Card title="Sereja Ris" image={sereja} onToggleFocus={toggleFocus} />
+        <Card title="Husen Siraaj" image={husen} onToggleFocus={toggleFocus} />
+        <Card title="Houston Ray" image={houston} onToggleFocus={toggleFocus} />
         <Card
           title="Dimitar Donovski"
           image={dimitar}
-          onToggleFocus={onToggleFocus}
+          onToggleFocus={toggleFocus}
         />
       </motion.div>
     </Workspace>
